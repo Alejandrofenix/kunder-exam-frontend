a9aee3431f8d6bc10e47bd7f133529d6f0a25b83
Alejandro Pinedo B <apinedobarrios@live.com>
2021-08-25 00:27:39 -0500

    add banner service and banner interface

--- src/app/interfaces/banner.interface.ts
+++ src/app/interfaces/banner.interface.ts
@@ -1,3 +1,27 @@
-export interface Banner {
 
+export interface Banner {
+ product: Product;
+}
+
+export interface Product {
+ show:               boolean;
+ type:               string;
+ backgroundImg:      string;
+ buttonText:         string;
+ link:               string;
+ productId:          string;
+ productImg:         string;
+ productName:        string;
+ productDescription: string;
+}
+
+// Converts JSON strings to/from your types
+export class Convert {
+ public static toBanner(json: string): Banner {
+     return JSON.parse(json);
+ }
+
+ public static bannerToJson(value: Banner): string {
+     return JSON.stringify(value);
+ }
 }
--- src/app/services/banner.service.ts
+++ src/app/services/banner.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, } from '@angular/common/http';
+import { Banner } from '../interfaces/banner.interface';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BannerService {
+  private _apiUrl: string = 'https://hack.kunderlabs.com/exam/telecom/api/products/new'
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getBannerInfo(): Observable<Banner> {
+
+    //This get return a Observable type Banner, this banner include a product.
+    return this.http.get<Banner>(this._apiUrl);
+  }
 }
