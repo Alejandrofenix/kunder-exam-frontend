e3c7b139aa3697b9aa338df5b855e7ac9f49675a
Alejandro Pinedo B <apinedobarrios@live.com>
2021-08-25 02:47:16 -0500

    Fix home route in heroku

--- server.js
+++ server.js
@@ -7,5 +7,7 @@ app.use(express.static(__dirname+'/dist/Kunder-exam'));
 app.get('/',function(req,res){
     res.sendFile(path.join(__dirname+'/dist/Kunder-exam/index.html'));
 });
-
+app.get('/home',function(req,res){
+    res.sendFile(path.join(__dirname+'/dist/Kunder-exam/index.html'));
+});
 app.listen(process.env.PORT || 8080);
